1a94e852aaf5801b4734c10c511d19c416841b48
Guilherme Queiroz Ribeiro <guilherme.q.r@hotmail.com>
2025-11-21 22:53:23 -0300

    atualizando dependencias

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,9 +6,7 @@ import FlutterMacOS
 import Foundation
 
 import brazilian_locations
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   BrazilianLocationsPlugin.register(with: registry.registrar(forPlugin: "BrazilianLocationsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
